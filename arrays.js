05f97f80f22b4869d2137f3fb216bd0b2e79aacd
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-25 01:21:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray (array1,elem1) {
+  array1 = [elem1,...array1]
+  return array1
+}
+
+function destructivelyAddElementToBeginningOfArray (array2,elem2) {
+  array2.
+}

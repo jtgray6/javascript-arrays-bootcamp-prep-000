ba1accb0928dcde38091ab4c2925e3cafc2c1d51
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-25 01:21:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,6 @@ function addElementToBeginningOfArray (array1,elem1) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array2,elem2) {
-  array2.
+  array2.unshift(elem2)
+  return array2
 }

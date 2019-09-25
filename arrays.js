bf9dfbee07eff828b2a8ceb854a1fb979d3f6bcc
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-25 01:24:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray (array2,elem2) {
   array2.unshift(elem2)
   return array2
 }
+
+function addElementToEndOfArray (array3,elem3) {
+  array3 = [...array3,elem3]
+  return array3
+}
+
+function destructivelyAddElementToEndOfArray (array4,elem4) {
+  array4.push(elem4)
+  return array4
+}
